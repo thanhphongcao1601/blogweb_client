302808e36b29053be2b91ae2c171a94cf56ddfd9
Phong Cao <thanhphongcao1601@gmail.com>
2022-09-27 22:11:42 +0700

    update base url

--- src/helper/constant.ts
+++ src/helper/constant.ts
@@ -1,5 +1,7 @@
 export class AppSettings {
-  static readonly BASE_URL = "http://localhost:8000/api/v1";
-  static readonly LOGIN_URL = "http://localhost:8000/api/v1/auth/login";
-  static readonly REGISTER_URL = "http://localhost:8000/api/v1/auth/register";
+  static readonly BASE_URL = "https://simpleblogweb.herokuapp.com/api/v1";
+  static readonly LOGIN_URL =
+    "https://simpleblogweb.herokuapp.com/api/v1/auth/login";
+  static readonly REGISTER_URL =
+    "https://simpleblogweb.herokuapp.com/api/v1/auth/register";
 }
